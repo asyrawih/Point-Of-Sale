195120634463e71f9da422bea468b6ef6f26bdc0
asyrawih <asyrawih@gmail.com>
2025-03-23 13:09:52 +0700

    fix(sidbar): sidebar not showed

--- src/components/ui/layout/MainLayout.tsx
+++ src/components/ui/layout/MainLayout.tsx
@@ -1,12 +1,14 @@
 import { AppTitleBar } from '../menu/AppTitleBar';
 import { MenuGrid } from '../menu/MenuGrid';
 import { OrderPanel } from './OrderPanel';
+import { Sidebar } from './Sidebar';
 
 export const MainLayout = () => {
     return (
         <div className="h-screen flex flex-col">
             <AppTitleBar />
             <div className="flex-1 flex">
+                <Sidebar />
                 <div className="flex-1 overflow-y-auto">
                     <MenuGrid />
                 </div>
